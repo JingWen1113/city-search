3d6040029885afb72fdc4f28533ce28e573f51f2
Wilfred0zz <wilfredo.recinos52@myhunter.cuny.edu>
2020-06-29 14:54:52 -0400

    added calls to api and store the zip codes in an array

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -9,16 +9,58 @@ class Search extends Component{
             Exist: false//sees if zipCodes exists
         }
     }
+
+    onChange = (event) =>{
+        let userCity = event.target.value;
+        let UppercasedCity= userCity.toUpperCase();
+        console.log(UppercasedCity);
+        this.setState({City : UppercasedCity})
+    }
+
+    onSubmit = async(event) => {
+        event.preventDefault()
+        if(this.state.City === "" || !this.state.City){
+            return(alert('Need a city to search zips!'))
+        }
+        if(!/^[a-zA-Z]+$/.test(this.state.City)){
+            return(alert('This is not a city!'))
+        }
+        this.fetchZipCodes()
+        console.log("calling fetch to api")
+    }
+
+    fetchZipCodes = async() =>{
+        try{
+            let CityGiven = this.state.City;
+            console.log('this is the City: '+ CityGiven);
+            let response = await fetch('http://ctp-zip-api.herokuapp.com/city/'+CityGiven, { method: 'GET'} )
+            console.log(response);
+            const status = response.status
+            let result = await response.json();
+            console.log(result);
+            if (status === 400 || status === 500) {
+                console.log(result.error)
+              } else {
+                this.setState({AllZips:result.data, Exist:true})//updates array and makes it so if the address is real Exist is true. 
+                console.log('array of state is updated')
+            }
+
+        }catch (error){
+            console.log(error);
+        }
+    }
+
+
     render(){
         const {City}=this.state.City
         console.log(this.state.City);
         return(
-            <di>
+            <div>
                 <form onSubmit={this.onSubmit}>
-                    <input type="text" onChange={this.onChange} value={Zip}></input>
+                    <input type="text" onChange={this.onChange} value={City}></input>
                     <input type="submit"></input>
                 </form>
-            </di>
+            </div>
         )
     }
 }
